299378bad86f585c010f28528bdf5299ccf07852
Henrique Oliveira | Desenvolvedor <henriqueoliver.m.s@gmail.com>
2026-09-07 15:58:29 +0000

    Adiciona variáveis e tipos

---
+++ 02_variaveis_tipos/variaveis_tipos_primitivos.py
@@ -0,0 +1,75 @@
+"""Variáveis e tipos primitivos."""
+
+
+
+_numero = 1
+
+Numero = 2
+
+numero = 3
+
+numero123 = 4
+
+print(_numero, Numero, numero, numero123)
+
+numero = 10
+
+print(numero)
+
+numero = 20
+
+print(numero)
+
+texto = 'Olá mundo!'
+
+print(texto)
+
+variavel = None
+
+print(variavel)
+
+inteiro = 10
+
+print(inteiro)
+
+decimal = 1.53
+
+print(decimal)
+
+texto = "olá, isto é um texto"
+
+print(texto)
+
+var = True
+
+print(var)
+
+var = False
+
+print(var)
+
+var1 = 10
+
+var2 = var1
+
+print(var2)
+
+saldo_bancario = 100
+
+print(saldo_bancario)
+
+inteiro, decimal, text, booleano = 750, 3.1415, "Maça", True
+
+print(inteiro, decimal, text, booleano, sep="\n")
+
+nome = "Fernando Amaral"
+
+CPF = "123.456.789-10"
+
+casado = True
+
+print("Nome: ", nome)
+
+print("CPF: ", CPF)
+
+print("Casado: ", casado)
