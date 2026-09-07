5bd0eb5de6385bf0f9dcca0a93ba37e5175c7d7c
Henrique Oliveira | Desenvolvedor <henriqueoliver.m.s@gmail.com>
2026-09-07 15:55:04 +0000

    Organiza código por tópicos

---
+++ 01_introducao/comentarios_print.py
@@ -0,0 +1,57 @@
+"""Introdução: comentários, print e formatação básica."""
+
+
+
+## Comentários
+
+
+
+# isto é um comentário
+
+# isto é outro comentário
+
+print("olá mundo") # isto é um comentário
+
+
+
+''' Este é um comentário
+
+o comentário continua aqui
+
+aqui ainda continua
+
+e aqui termina '''
+
+
+
+print('olá mundo')
+
+print(10)
+
+print(20.5)
+
+print("Maça", 20, 30.45)
+
+print("Maça", "Pera", "Uva", sep=' - ')
+
+print("Maça","Pera", end = ' Fim', sep=' ')
+
+print("Este é um texto longo, \n eu quero quebrar a linha")
+
+print("Maça","Pera", sep='\n')
+
+print("A pontuação total de %s foi %s pontos" % ("Fernando","10") )
+
+print("A pontuação total de {} foi {} pontos".format("Fernando","10"))
+
+print("A pontuação total de" + " Fernando" + " foi" + " 10 " + "pontos")
+
+print("Fernando")
+
+print("30")
+
+print("1,92")
+
+print("Nome: {}, idade:{}, altura:{}".format("Fernando", "30", "1,90"))
+
+print(1,2,3, sep="-")
